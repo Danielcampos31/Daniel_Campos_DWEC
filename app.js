d39bf402632bc3e6521eacbae2b18a0f10181b40
Daniel <campospena.daniel@jgvaldemora.org>
2026-09-23 10:19:52 +0200

    Añadido contador al boton

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("¡Hola DWEC! Mi taller ya funciona.");
+
+let boton = document.getElementById("boton");
+let aviso = document.getElementById("aviso");
+
+let contador = 0;
+
+boton.addEventListener("click", function() {
+
+    contador++;
+
+    aviso.textContent = "Has pulsado el botón " + contador + " veces";
+
+});
